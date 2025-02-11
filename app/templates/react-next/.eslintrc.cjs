648726b0091d8eadfdc4d13dfdf0304033039688
AnthoineSG <75179286+AnthoineSG@users.noreply.github.com>
2025-02-11 10:52:30 +0100

    feat: add template for react nextjs

---
+++ app/templates/react-next/.eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prefer-arrow', 'prettier'],
+  rules: {
+    'prettier/prettier': ['warn'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prefer-arrow/prefer-arrow-functions': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: '{react,react-dom/**}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
---
+++ app/templates/react-next/.prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  jsxBracketSameLine: false,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  semi: true,
+  endOfLine: 'auto',
+};
